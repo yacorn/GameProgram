985cab8500022f2c00a80d529f8232d608d62fd0
yacorn <kogizoahae@gmail.com>
2025-12-16 19:45:41 +0900

    (2025.12.16) User Interface

---
+++ Assets/Class5th (User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void GameStartButton()
+    {
+        Debug.Log("Start");
+    }
+    
+    public void GameOptionsButton()
+    {
+        Debug.Log("Options");
+    }
+
+    public void GameExitButton()
+    {
+        Debug.Log("Exit");
+    }
+}
